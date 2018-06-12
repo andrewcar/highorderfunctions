be6843ee4dd96e889d8daf99b7f08a643b88b32a
Andrew Carvajal <andrewcarvajal1@gmail.com>
2018-06-12 17:16:20 -0400

    Added MergeSort.playground and the beginning of MostCommonElement.playground

---
+++ MergeSort.playground/Contents.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+////////////////////////
+//////MERGE/////////////
+///////////SORT/////////
+////////////////////////
+
+// SPLIT ARRAYS
+
+func mergeSort(array: [Int]) -> [Int] {
+    
+    guard array.count > 1 else { return array }
+    
+    let leftArray = Array(array[0..<array.count/2])
+    let rightArray = Array(array[array.count/2..<array.count])
+    
+    return merge(left: mergeSort(array: leftArray), right: mergeSort(array: rightArray))
+}
+
+// MERGE
+
+func merge(left: [Int], right: [Int]) -> [Int] {
+    
+    var mergedArray: [Int] = []
+    var left = left
+    var right = right
+    
+    while left.count > 0 && right.count > 0 {
+        
+        if left.first! < right.first! {
+            mergedArray.append(left.removeFirst())
+        } else {
+            mergedArray.append(right.removeFirst())
+        }
+    }
+    
+    return mergedArray + left + right
+}
+
+// TEST
+
+var randomInts: [Int] = []
+
+for _ in 0..<50 {
+    let randomInt = Int(arc4random_uniform(UInt32(1000)))
+    randomInts.append(randomInt)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MostCommonElement.playground/Contents.swift
@@ -0,0 +1,3 @@
+import Foundation
+
+var colors = ["red", "green", "blue", "red", "blue", "cyan", "orange", "red", "red", "cyan", "blue", "yellow", "blue", "orange", "purple", "green", "purple", "purple", "cyan", "red", "blue", "orange", "green", "red", "purple", "cyan"]
