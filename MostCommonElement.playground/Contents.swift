4e2a8715c826ddc8bf73204d637d295db19d30b4
Andrew Carvajal <andrewcarvajal1@gmail.com>
2018-06-12 22:55:13 -0400

    Completed MostCommonElement

--- MostCommonElement.playground/Contents.swift
+++ MostCommonElement.playground/Contents.swift
@@ -1,3 +1,45 @@
 import Foundation
 
-var colors = ["red", "green", "blue", "red", "blue", "cyan", "orange", "red", "red", "cyan", "blue", "yellow", "blue", "orange", "purple", "green", "purple", "purple", "cyan", "red", "blue", "orange", "green", "red", "purple", "cyan"]
+////////////////////////
+//////MOST//COMMON//////
+////////ELEMENT/////////
+////////////////////////
+
+var colors = ["red", "green", "blue", "red", "blue", "cyan", "orange", "red", "red", "cyan", "blue", "yellow", "blue", "orange", "purple", "green", "purple", "purple", "cyan", "red", "blue", "orange", "green", "red", "purple", "cyan", "purple", "purple"]
+
+func getMostCommonColor(array: [String]) -> [String] {
+
+    var topColors: [String] = []
+    var colorDictionary: [String: Int] = [:]
+
+    // ADD COLOR COUNTS TO DICTIONARY
+    for color in array {
+        if let count = colorDictionary[color] {
+            colorDictionary[color] = count + 1
+        } else {
+            colorDictionary[color] = 1
+        }
+    }
+
+    // GET HIGHEST COUNT
+    let highestValue = colorDictionary.values.max()
+
+    // ADD COLOR(S) WITH HIGHEST VALUE TO ARRAY
+    for (color, count) in colorDictionary {
+        if count == highestValue {
+            topColors.append(color)
+        }
+    }
+
+    /* OR USE FILTER
+    let filteredColorDictionary = colorDictionary.filter { (color, count) -> Bool in
+        return count == highestValue
+    }
+    topColors = filteredColorDictionary.map{$0.key}
+     */
+
+    return topColors
+}
+
+getMostCommonColor(array: colors)
+
