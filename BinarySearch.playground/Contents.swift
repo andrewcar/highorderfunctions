6e8437cbe32e1b11d9821b1230bc53ba8f733e13
Andrew Carvajal <andrewcarvajal1@gmail.com>
2018-06-14 02:50:16 -0400

    Completed BinarySearch and added Brute Force method of TwoSumProblem

---
+++ BinarySearch.playground/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+////////////////////////
+////////BINARY//////////
+////////SEARCH//////////
+////////////////////////
+
+let numbers = [1, 4, 6, 7, 10, 12, 18, 19, 23, 24, 34, 44, 58, 65]
+
+func binarySearch(array: [Int], key: Int) -> Bool {
+    
+    if array.count == 0 { return false }
+    
+    let minIndex = 0
+    let maxIndex = array.count - 1
+    let midIndex = maxIndex/2
+    let midValue = array[midIndex]
+    
+    if key < array[minIndex] || key > array[maxIndex] {
+        return false
+    }
+    
+    if key > midValue {
+        let slice = Array(array[midIndex + 1...maxIndex])
+        return binarySearch(array: slice, key: key)
+    }
+    
+    if key < midValue {
+        let slice = Array(array[minIndex...midIndex - 1])
+        return binarySearch(array: slice, key: key)
+    }
+    
+    if key == midValue {
+        return true
+    }
+    
+    return false
+}
+
+binarySearch(array: numbers, key: 23)
--- TwoSumProblem.playground/Contents.swift
+++ TwoSumProblem.playground/Contents.swift
@@ -5,4 +5,26 @@ import Foundation
 ////////PROBLEM/////////
 ////////////////////////
 
+// return a boolean value if there are two numbers in the array that equal a sum
 
+let numbers = [1, 3, 6, 7, 7, 12, 14]
+
+// 1. Brute Force - Nested For-Loop compare all (n2)
+
+func bruteForce(array: [Int], sum: Int) -> Bool {
+    
+    for i in 0..<array.count {
+        for j in 0..<array.count where j != i {
+            if array[j] + array[i] == sum {
+                print("TRUE: \(array[j]) + \(array[i])")
+                return true
+            } else {
+                print("FALSE: \(array[j]) + \(array[i])")
+            }
+        }
+    }
+    
+    return false
+}
+
+bruteForce(array: numbers, sum: 2)
