2b603347390e29645dee86554647af2d0e166f57
Andrew Carvajal <andrewcarvajal1@gmail.com>
2018-06-14 19:18:24 -0400

    Added n log n solution to Two Sum Problem

--- TwoSumProblem.playground/Contents.swift
+++ TwoSumProblem.playground/Contents.swift
@@ -9,17 +9,14 @@ import Foundation
 
 let numbers = [1, 3, 6, 7, 7, 12, 14]
 
-// 1. Brute Force - Nested For-Loop compare all (n2)
+// 1. Brute Force - Nested For-Loop to compare all (n2)
 
 func bruteForce(array: [Int], sum: Int) -> Bool {
 
     for i in 0..<array.count {
         for j in 0..<array.count where j != i {
             if array[j] + array[i] == sum {
-                print("TRUE: \(array[j]) + \(array[i])")
                 return true
-            } else {
-                print("FALSE: \(array[j]) + \(array[i])")
             }
         }
     }
@@ -28,3 +25,60 @@ func bruteForce(array: [Int], sum: Int) -> Bool {
 }
 
 bruteForce(array: numbers, sum: 2)
+
+// 2. Binary search for complement (n log n)
+
+func binarySearch(array: [Int], key: Int) -> Bool {
+    
+    if array.count == 0 { return false }
+    
+    let minIndex = 0
+    let maxIndex = array.count - 1
+    let midIndex = maxIndex/2
+    let midValue = array[midIndex]
+    
+    if key < array[minIndex] || key > array[maxIndex] {
+        return false
+    }
+    
+    if key < midValue {
+        let slice = Array(array[0...midIndex - 1])
+        return binarySearch(array: slice, key: key)
+    }
+    
+    if key > midValue {
+        let slice = Array(array[midIndex + 1...maxIndex])
+        return binarySearch(array: slice, key: key)
+    }
+    
+    if key == midValue {
+        return true
+    }
+    return false
+}
+
+func twoSumBinarySearch(array: [Int], sum: Int) -> Bool {
+    
+    if array.count == 0 { return false }
+    
+    for i in 0..<array.count {
+        let complement = sum - array[i]
+        var tempArray = array
+        tempArray.remove(at: i)
+        
+        let hasComplement = binarySearch(array: tempArray, key: complement)
+        if hasComplement {
+            return true
+        }
+    }
+    return false
+}
+
+twoSumBinarySearch(array: numbers, sum: 15)
+
+
+
+
+
+
+
