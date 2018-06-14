77aac436a05d036c32acd8916dbc203e48c1e973
Andrew Carvajal <andrewcarvajal1@gmail.com>
2018-06-14 12:53:39 -0400

    Completed BinarySearch

--- TwoSumProblem.playground/Contents.swift
+++ TwoSumProblem.playground/Contents.swift
@@ -12,7 +12,7 @@ let numbers = [1, 3, 6, 7, 7, 12, 14]
 // 1. Brute Force - Nested For-Loop compare all (n2)
 
 func bruteForce(array: [Int], sum: Int) -> Bool {
-    
+
     for i in 0..<array.count {
         for j in 0..<array.count where j != i {
             if array[j] + array[i] == sum {
@@ -23,7 +23,7 @@ func bruteForce(array: [Int], sum: Int) -> Bool {
             }
         }
     }
-    
+
     return false
 }
 
